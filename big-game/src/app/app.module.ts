312cd3e59890787ab93712377ceab523abd8b6ad
lalaantika <sharmal0707@gmail.com>
2020-05-04 13:55:19 -0400

    created all the components

--- big-game/src/app/app.module.ts
+++ big-game/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HorsePageComponent } from './horse-page/horse-page.component';
+import { StorePageComponent } from './store-page/store-page.component';
+import { BlackMarketPageComponent } from './black-market-page/black-market-page.component';
+import { CompetitionPageComponent } from './competition-page/competition-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlayBarComponent } from './play-bar/play-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SignUpComponent,
+    HorsePageComponent,
+    StorePageComponent,
+    BlackMarketPageComponent,
+    CompetitionPageComponent,
+    NavBarComponent,
+    FooterComponent,
+    PlayBarComponent
   ],
   imports: [
     BrowserModule,
