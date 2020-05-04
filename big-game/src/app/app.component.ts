84d8866c09a3e11594efec08dd246567d84639af
burningman2k20 <burningman2k9@gmail.com>
2020-05-04 15:02:10 -0400

    updated

--- big-game/src/app/app.component.ts
+++ big-game/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -12,19 +11,5 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent {
   title = 'big-game';
   public isCollapsed = false;
-  public rate=10;
-
-  ctrl = new FormControl(null, Validators.required);
-
-  public readonly = true;
-  
-  //this.ctrl.disable();
-
-  toggle() {
-    if (this.ctrl.disabled) {
-      this.ctrl.enable();
-    } else {
-      this.ctrl.disable();
-    }
-  }
+ 
 };
--- big-game/src/app/horse-page/horse-page.component.ts
+++ big-game/src/app/horse-page/horse-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-horse-page',
@@ -10,6 +12,22 @@ export class HorsePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public rate=10;
+
+  ctrl = new FormControl(null, Validators.required);
+
+  public readonly = true;
+  
+  //this.ctrl.disable();
+
+  toggle() {
+    if (this.ctrl.disabled) {
+      this.ctrl.enable();
+    } else {
+      this.ctrl.disable();
+    }
   }
 
 }
