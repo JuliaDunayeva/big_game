312d079e4a2d1761cfdcc1bc8d5707b4aed374f4
mojtabafazeli <fazeli.mojtaba@gmail.com>
2020-05-12 15:25:09 -0400

    pictures fixed

---
+++ big-game/src/app/sign-up/breed.ts
@@ -0,0 +1,11 @@
+export class Breed {
+    key: string;
+    breed: string;
+    breed_id: number;
+    constructor(key:string, breed: string, breed_id:number) {
+        this.key = key;
+        this.breed = breed;
+        this.breed_id = breed_id;
+    }   
+
+}
