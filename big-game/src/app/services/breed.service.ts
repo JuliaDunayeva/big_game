679361bca32661da6bbf48d54a65cca54cb7f474
mojtabafazeli <fazeli.mojtaba@gmail.com>
2020-05-13 11:11:49 -0400

    adding database

--- big-game/src/app/services/breed.service.ts
+++ big-game/src/app/services/breed.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BreedService {
 
-  constructor() { }
+  constructor(public db: AngularFirestore) { }
+
+  getBreeds() {
+    return this.db.collection('/breed').valueChanges()
+  }
 }
