175efa089f6241bfef32043ea78979e9ba61dc43
burningman2k20 <burningman2k9@gmail.com>
2020-05-19 14:41:30 -0400

    more clean up of code

--- big-game/src/app/horse-page/horse-page.component.ts
+++ big-game/src/app/horse-page/horse-page.component.ts
@@ -1,3 +1,4 @@
+//All class imports
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -24,111 +25,109 @@ import { map } from 'rxjs/operators';
 })
 
 export class HorsePageComponent implements OnInit {
-colors: Color[] = [];
-allColors: Color[];
-allBreeds: Breed[];
-allSkills: string[];
-allHorseData: HorseData[];
-skill: string;
-userData: UserData[];
+	colors: Color[] = [];
+	allColors: Color[];
+	allBreeds: Breed[];
+	allSkills: string[];
+	allHorseData: HorseData[];
+	skill: string;
+	userData: UserData[];
 
-public isCareCollapsed = false;
-public isRideCollapsed = false;
+	public isCareCollapsed = false;
+	public isRideCollapsed = false;
 
-public isNightCollapsed = false;
-public isTrainingCollapsed = false;
+	public isNightCollapsed = false;
+	public isTrainingCollapsed = false;
 
-public isECCollapsed = false;
-public isCompetitionCollapsed = false;
+	public isECCollapsed = false;
+	public isCompetitionCollapsed = false;
 
-public isHistoryCollapsed = false;
-public isBreedingCollapsed = false;
+	public isHistoryCollapsed = false;
+	public isBreedingCollapsed = false;
 
-public isMiddleCollapsed =false;
+	public isMiddleCollapsed =false;
 
-ctrl = new FormControl(null, Validators.required);
+	ctrl = new FormControl(null, Validators.required);
 
-public readonly = true;
-public value = 0;
-public userIndex=1;
+	public readonly = true;
+	public value = 0;
+	public userIndex=1;
     
   constructor(private router: Router, 
-    private http: HttpClient,
-    public colorService: ColorService, 
-    public breedService: BreedService,
-    public userDataService: UserDataService,
-    public HorseDataService: HorseDataService) { }
+	private http: HttpClient,
+	public colorService: ColorService, 
+	public breedService: BreedService,
+	public userDataService: UserDataService,
+	public HorseDataService: HorseDataService) { }
   
-	createdata(){
-		this.userDataService.createdata();
-  		console.log('add user');
-	}
-	  
-    ngOnInit(): void {
-    
-      this.getBreeds();
-      this.getColors();   
-      this.getUserData(); 
-      this.getHorseData();
-  }
-  
-  toggle() {
-    if (this.ctrl.disabled) {
-      this.ctrl.enable();
-    } else {
-      this.ctrl.disable();
-    }
-  }
-
-  getBreeds(): Breed[]{
-    this.breedService.getBreeds().subscribe(
-      result => {
-        console.log(result);
-        this.allBreeds = result as Array<Breed>;
-        console.log(this.allBreeds[0].skill);
-      }
-    )
-    return this.allBreeds;
-  }
-
-  getColors(): Color[] {
-    this.colorService.getColors().subscribe(
-      result =>{
-        console.log(result);
-        this.allColors = result as Array<Color>;
-        console.log(this.allColors[0].color)
-      }
-    )
-    return this.colors;
-  }
-
-  getUserData(): UserData[] {
-    this.userDataService.getUserData().subscribe(
-      result =>{
-        console.log(result);
-        this.userData = result as Array<UserData>;
-        console.log(this.userData[0].userName);
-      }
-    )
-    return this.userData;
-  }
-
-  getHorseData(): HorseData[] {
-    this.HorseDataService.getHorseData().subscribe(
-      result =>{
-        console.log(result);
-        this.allHorseData = result as Array<HorseData>;
-        console.log(this.allHorseData[0].stamina)
-      }
-    )
-    return this.allHorseData;
-  }
- 
- public beforeChange($event: NgbPanelChangeEvent) {
-
-    if ($event.panelId === 'preventchange' && $event.nextState === false) {
-      $event.preventDefault();
-    }
-  }
-
+createdata(){
+	this.userDataService.createdata();
+	console.log('add user');
 }
+	  
+ngOnInit(): void {
+	this.getBreeds();
+	this.getColors();   
+	this.getUserData(); 
+	this.getHorseData();
+}
+  
+toggle() {
+	if (this.ctrl.disabled) {
+		this.ctrl.enable();
+	} else {
+      		this.ctrl.disable();
+	}
+}
+
+getBreeds(): Breed[]{
+	this.breedService.getBreeds().subscribe(
+		result => {
+        		console.log(result);
+        		this.allBreeds = result as Array<Breed>;
+			console.log(this.allBreeds[0].skill);
+		}
+	)
+    return this.allBreeds;
+}
+
+getColors(): Color[] {
+	this.colorService.getColors().subscribe(
+      		result =>{
+        		console.log(result);
+        		this.allColors = result as Array<Color>;
+        		console.log(this.allColors[0].color)
+      		}
+    	)
+    return this.colors;
+}
+
+getUserData(): UserData[] {
+	this.userDataService.getUserData().subscribe(
+      		result =>{
+			console.log(result);
+        		this.userData = result as Array<UserData>;
+        		console.log(this.userData[0].userName);
+      		}
+    	)
+    return this.userData;
+}
+
+getHorseData(): HorseData[] {
+	this.HorseDataService.getHorseData().subscribe(
+      		result =>{
+        		console.log(result);
+        		this.allHorseData = result as Array<HorseData>;
+        		console.log(this.allHorseData[0].stamina)
+      		}
+    	)
+    return this.allHorseData;
+}
+ 
+public beforeChange($event: NgbPanelChangeEvent) {
+	if ($event.panelId === 'preventchange' && $event.nextState === false) {
+		$event.preventDefault();
+    	}
+}
+
+}  //end of  class definition
