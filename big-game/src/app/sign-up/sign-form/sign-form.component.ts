a5cc0cdf52bd29b4814a5f6975fe53fd6f90cb0c
aus <ausalmadi@gmail.com>
2020-05-19 13:18:17 -0400

    no message

--- big-game/src/app/sign-up/sign-form/sign-form.component.ts
+++ big-game/src/app/sign-up/sign-form/sign-form.component.ts
@@ -10,33 +10,32 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class SignFormComponent implements OnInit {
-  signForm: FormGroup 
-//   public user: UserData
-   submitted = false;
-//   private http: HttpClient;
-contactForm: FormGroup;
+  signForm: FormGroup
+  //   public user: UserData
+  submitted = false;
+  //   private http: HttpClient;
+  contactForm: FormGroup;
 
-  
-   constructor() {
+  constructor() {
     this.signForm = this.signUpForm();
-    }
+  }
 
   ngOnInit(): void {
-    
 
-}
-   signUpForm(): FormGroup
- {
+  }
+  signUpForm(): FormGroup {
     return new FormGroup({
-      userData : new FormGroup({username : new FormControl(null, [Validators.required, Validators.minLength(4)]),
-        email : new FormControl(null, [Validators.required, Validators.email,]),
+      userData: new FormGroup({
+        username: new FormControl(null, [Validators.required, Validators.minLength(4)]),
+        email: new FormControl(null, [Validators.required, Validators.email,]),
         password: new FormControl(null, [Validators.required]),
-        confirmpassword: new FormControl(null, [Validators.required])})
-      
+        confirmpassword: new FormControl(null, [Validators.required])
+      })
+
     })
- }
-  onSubmit(){
+  }
+  onSubmit() {
     // console.log(this.signForm)
-    
+
   }
-  }
+}
