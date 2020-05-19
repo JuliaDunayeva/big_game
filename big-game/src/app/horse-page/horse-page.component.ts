a87467637c366b1421b51af063ddb3016c05f0c0
mojtabafazeli <fazeli.mojtaba@gmail.com>
2020-05-19 15:01:02 -0400

    Revert "Merge pull request #79 from JuliaDunayeva/BK-9-Hourse"
    This reverts commit 0e9175410c9b90428412bf6c8d2d096f7eda449f, reversing
    changes made to 67fe650a20eba23a806266f24f879ce50ed3a91d.

--- big-game/src/app/horse-page/horse-page.component.ts
+++ big-game/src/app/horse-page/horse-page.component.ts
@@ -1,9 +1,12 @@
-//All class imports
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+//import { SignUpComponent } from '../sign-up/sign-up.component'
+
+//import { HorseCollection } from './horse-collection';
 import { HttpClient } from '@angular/common/http';
 import { getLocaleDateFormat } from '@angular/common';
 import { Color } from '../color';
@@ -12,11 +15,6 @@ import { BreedService } from '../services/breed.service';
 import { Breed } from '../breed';
 import { UserData } from '../user-data';
 import { UserDataService } from '../services/user-data.service';
-import { HorseData } from '../horse-data';
-import { HorseDataService} from '../services/horse-data.service';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-horse-page',
@@ -25,109 +23,110 @@ import { map } from 'rxjs/operators';
 })
 
 export class HorsePageComponent implements OnInit {
-	colors: Color[] = [];
-	allColors: Color[];
-	allBreeds: Breed[];
-	allSkills: string[];
-	allHorseData: HorseData[];
-	skill: string;
-	userData: UserData[];
 
-	public isCareCollapsed = false;
-	public isRideCollapsed = false;
+  colors: Color[] = [];
+  allColors: Color[];
+  allBreeds: Breed[];
+  allSkills: string[];
+  skill: string;
 
-	public isNightCollapsed = false;
-	public isTrainingCollapsed = false;
-
-	public isECCollapsed = false;
-	public isCompetitionCollapsed = false;
-
-	public isHistoryCollapsed = false;
-	public isBreedingCollapsed = false;
-
-	public isMiddleCollapsed =false;
-
-	ctrl = new FormControl(null, Validators.required);
-
-	public readonly = true;
-	public value = 0;
-	public userIndex=1;
-    
+  userData: UserData[];
+  //UserDataService: any;
+  
   constructor(private router: Router, 
-	private http: HttpClient,
-	public colorService: ColorService, 
-	public breedService: BreedService,
-	public userDataService: UserDataService,
-	public HorseDataService: HorseDataService) { }
-  
-createdata(){
-	this.userDataService.createdata();
-	console.log('add user');
-}
-	  
-ngOnInit(): void {
-	this.getBreeds();
-	this.getColors();   
-	this.getUserData(); 
-	this.getHorseData();
-}
-  
-toggle() {
-	if (this.ctrl.disabled) {
-		this.ctrl.enable();
-	} else {
-      		this.ctrl.disable();
-	}
-}
+    private http: HttpClient,
+    public colorService: ColorService, 
+    public breedService: BreedService,
+    public userDataService: UserDataService) { }
 
-getBreeds(): Breed[]{
-	this.breedService.getBreeds().subscribe(
-		result => {
-        		console.log(result);
-        		this.allBreeds = result as Array<Breed>;
-			console.log(this.allBreeds[0].skill);
-		}
-	)
+  ngOnInit(): void {
+    //SignUpComponent.getColors();
+this.getBreeds();
+this.getColors();   
+this.getUserData(); 
+    //SignUpComponent.getBreeds();
+  }
+  public isLCollapsed = false;
+  public isRCollapsed = false;
+
+  public isLCollapsed1 = false;
+  public isRCollapsed1 = false;
+
+  public isLCollapsed2 = false;
+  public isRCollapsed2 = false;
+
+  public isLCollapsed3 = false;
+  public isRCollapsed3 = false;
+
+  public isLCollapsed4 = false;
+  public isRCollapsed4 = false;
+
+  public isMCollapsed =false;
+
+  public rate=10;
+  
+  active = 1;
+
+  ctrl = new FormControl(null, Validators.required);
+
+  public preventchange_1:true;
+
+  public readonly = true;
+
+  public value = 0;
+  
+  //this.ctrl.disable();
+
+  toggle() {
+    if (this.ctrl.disabled) {
+      this.ctrl.enable();
+    } else {
+      this.ctrl.disable();
+    }
+  }
+
+  getBreeds(): Breed[]{
+    this.breedService.getBreeds().subscribe(
+      result => {
+        console.log(result);
+        this.allBreeds = result as Array<Breed>;
+        // for (let i = 0 ; i < this.allBreeds.length ; i++) {
+        //   this.allSkills.push(this.allBreeds[i].skill)
+        // }
+        console.log(this.allBreeds[0].skill);
+        // console.log(this.allSkills);
+      }
+    )
     return this.allBreeds;
-}
+  }
 
-getColors(): Color[] {
-	this.colorService.getColors().subscribe(
-      		result =>{
-        		console.log(result);
-        		this.allColors = result as Array<Color>;
-        		console.log(this.allColors[0].color)
-      		}
-    	)
+  getColors(): Color[] {
+    this.colorService.getColors().subscribe(
+      result =>{
+        console.log(result);
+        this.allColors = result as Array<Color>;
+        console.log(this.allColors[0].color)
+      }
+    )
     return this.colors;
-}
+  }
 
-getUserData(): UserData[] {
-	this.userDataService.getUserData().subscribe(
-      		result =>{
-			console.log(result);
-        		this.userData = result as Array<UserData>;
-        		console.log(this.userData[0].userName);
-      		}
-    	)
+  getUserData(): UserData[] {
+    this.userDataService.getUserData().subscribe(
+      result =>{
+        console.log(result);
+        this.userData = result as Array<UserData>;
+        console.log(this.userData[0].password);
+      }
+    )
     return this.userData;
-}
-
-getHorseData(): HorseData[] {
-	this.HorseDataService.getHorseData().subscribe(
-      		result =>{
-        		console.log(result);
-        		this.allHorseData = result as Array<HorseData>;
-        		console.log(this.allHorseData[0].stamina)
-      		}
-    	)
-    return this.allHorseData;
-}
+  }
  
-public beforeChange($event: NgbPanelChangeEvent) {
-	if ($event.panelId === 'preventchange' && $event.nextState === false) {
-		$event.preventDefault();
-    	}
-}
+ public beforeChange($event: NgbPanelChangeEvent) {
 
-}  //end of  class definition
+    if ($event.panelId === 'preventchange_1' && $event.nextState === false) {
+      $event.preventDefault();
+    }
+  }
+
+}
--- big-game/src/app/services/user-data.service.ts
+++ big-game/src/app/services/user-data.service.ts
@@ -9,21 +9,10 @@ import { UserData } from '../user-data';
 })
 export class UserDataService {
 
-    constructor(public db: AngularFirestore) { }
+  constructor(public db: AngularFirestore) { }
 
-  createdata(){
-    let data = {
-      userName:'new testy splity 2',
-      name: 'Los Angeles',
-      state: 'CA',
-      country: 'USA'
-    };
-    
-    let setDoc = this.db.collection('testdata').doc("testuser2").set(data);
-
-  }
   getUserData() {
-    return this.db.collection('/testdata').valueChanges()
+    return this.db.collection('/user_data').valueChanges()
   }
   
 }
