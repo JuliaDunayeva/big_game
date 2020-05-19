8674070ae8121760907b0a1b0e891398ac4e677e
burningman2k20 <burningman2k9@gmail.com>
2020-05-19 13:39:16 -0400

    more clean up

--- big-game/src/app/services/user-data.service.ts
+++ big-game/src/app/services/user-data.service.ts
@@ -19,10 +19,8 @@ export class UserDataService {
       country: 'USA'
     };
     
-    // Add a new document in collection "cities" with ID 'LA'
     let setDoc = this.db.collection('testdata').doc("testuser2").set(data);
-    
-    //let setDoc = this.db.collection('testdata').doc('user').set(data);
+
   }
   getUserData() {
     return this.db.collection('/testdata').valueChanges()
--- big-game/src/app/sign-up/sign-up.component.ts
+++ big-game/src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HorseCollection } from './horse-collection';
 import { HttpClient } from '@angular/common/http';
 import { getLocaleDateFormat } from '@angular/common';
 import { Color } from '../color';
