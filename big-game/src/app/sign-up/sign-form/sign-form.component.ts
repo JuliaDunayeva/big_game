e07716380bf758e34a863c360723bfc0e343d525
aus <ausalmadi@gmail.com>
2020-05-19 13:22:36 -0400

    no message

--- big-game/src/app/sign-up/sign-form/sign-form.component.ts
+++ big-game/src/app/sign-up/sign-form/sign-form.component.ts
@@ -6,37 +6,34 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-sign-form',
   templateUrl: './sign-form.component.html',
-  styleUrls: ['./sign-form.component.css']
+  styleUrls: ['./sign-form.component.css'],
 })
-
 export class SignFormComponent implements OnInit {
-  signForm: FormGroup 
-//   public user: UserData
-   submitted = false;
-//   private http: HttpClient;
-contactForm: FormGroup;
+  signForm: FormGroup;
+  //   public user: UserData
+  submitted = false;
+  //   private http: HttpClient;
+  contactForm: FormGroup;
 
-  
-   constructor() {
+  constructor() {
     this.signForm = this.signUpForm();
-    }
+  }
 
-  ngOnInit(): void {
-    
-
-}
-   signUpForm(): FormGroup
- {
+  ngOnInit(): void {}
+  signUpForm(): FormGroup {
     return new FormGroup({
-      userData : new FormGroup({username : new FormControl(null, [Validators.required, Validators.minLength(4)]),
-        email : new FormControl(null, [Validators.required, Validators.email,]),
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
         password: new FormControl(null, [Validators.required]),
-        confirmpassword: new FormControl(null, [Validators.required])})
-      
-    })
- }
-  onSubmit(){
+        confirmpassword: new FormControl(null, [Validators.required]),
+      }),
+    });
+  }
+  onSubmit() {
     // console.log(this.signForm)
-    
   }
-  }
+}
