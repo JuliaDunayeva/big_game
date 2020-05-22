0dcae9d6f51079208b14b83d973d986b789c0bc9
lalaantika <sharmal0707@gmail.com>
2020-05-22 14:58:03 -0400

    login component with databse

--- big-game/src/app/home-page/login-btn/login-btn.component.ts
+++ big-game/src/app/home-page/login-btn/login-btn.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { UserDataService } from 'src/app/services/user-data.service';
 
 @Component({
   selector: 'app-login-btn',
@@ -9,13 +10,17 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginBtnComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private userService: UserDataService) {}
   
   ngOnInit(): void {
   }
   
   logIn(form: NgForm) {
-    this.router.navigate(['horse-page/:id'])
+    //this.router.navigate(['horse-page/:id'])
+    this.userService.logInUser(form).subscribe(res => {
+        console.log(JSON.stringify(res));
+    })
   }
 
 }
