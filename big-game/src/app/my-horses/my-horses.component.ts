d4de48c07aed8e34c58464418dfd4b501ab9d55c
burningman2k20 <burningman2k9@gmail.com>
2020-05-22 17:05:35 -0400

    Update my-horses.component.ts

--- big-game/src/app/my-horses/my-horses.component.ts
+++ big-game/src/app/my-horses/my-horses.component.ts
@@ -1,17 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { FormControl, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClient } from '@angular/common/http';
-import { UserData } from '../user-data';
 import { UserDataService } from '../services/user-data.service';
-import { HorseData } from '../horse-data';
-import { HorseDataService} from '../services/horse-data.service';
-=======
-import { UserDataService } from '../services/user-data.service';
->>>>>>> d150f4eafeae825261399df7dbc3a09837ab46b4
 
 @Component({
   selector: 'app-my-horses',
