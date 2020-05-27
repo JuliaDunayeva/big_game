481b7d22a7417b520c27d7146999e149c235e382
mojtabafazeli <fazeli.mojtaba@gmail.com>
2020-05-26 22:45:03 -0400

    breed select in competition fixed. value in option changed to ngValue

--- big-game/src/app/competition-page/competitions/competitions.component.ts
+++ big-game/src/app/competition-page/competitions/competitions.component.ts
@@ -3,37 +3,30 @@ import { Breed } from 'src/app/breed';
 import { BreedService } from 'src/app/services/breed.service';
 import { CompetitionService } from 'src/app/services/competition.service';
 
-
 @Component({
-  selector: 'app-competitions',
-  templateUrl: './competitions.component.html',
-  styleUrls: ['./competitions.component.css']
+	selector: 'app-competitions',
+	templateUrl: './competitions.component.html',
+	styleUrls: [ './competitions.component.css' ]
 })
 export class CompetitionsComponent implements OnInit {
+	allBreeds: Breed[];
+	breedSelected: string;
 
-  allBreeds: Breed[];
+	constructor(private breedService: BreedService, private competitionService: CompetitionService) {}
 
-  constructor(private breedService: BreedService, private competitionService: CompetitionService) { 
+	ngOnInit(): void {
+		this.getBreeds();
+	}
 
-  }
-
-  ngOnInit(): void {
-    this.getBreeds()
-    //this.competitionService.createCompetition("gervan")
-  }
-
-  getBreeds(): Breed[] {
+	getBreeds(): Breed[] {
 		this.breedService.getBreeds().subscribe((result) => {
 			console.log(result);
 			this.allBreeds = result as Array<Breed>;
 		});
 		return this.allBreeds;
-  }
-  
-  createCompetition(comp_name, breed) {
-    console.log(comp_name, breed)
-    this.competitionService.createCompetition(comp_name, breed)
-    
-  }
+	}
 
+	createCompetition(comp_name: string, breed: string) {
+		this.competitionService.createCompetition(comp_name, breed);
+	}
 }
--- big-game/src/app/services/competition.service.ts
+++ big-game/src/app/services/competition.service.ts
@@ -6,59 +6,52 @@ import { Compete } from '../compete';
 import { Breed } from '../breed';
 import { BreedService } from './breed.service';
 
-
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class CompetitionService {
+	comp_name: string;
+	difficulty: number;
+	energy: number;
+	kitty: number;
+	ranks: number;
+	over: boolean;
 
-  comp_name: string;
-  difficulty: number;
-  energy: number;
-  kitty: number;
-  ranks: number;
-  over: boolean;
-  
+	constructor(public db: AngularFirestore, private breedService: BreedService) {}
 
-  constructor(public db: AngularFirestore,
-              private breedService: BreedService) { }
+	getCompetitions() {
+		return this.db.collection('/competitions').valueChanges();
+	}
 
-  getCompetitions() {
-    return this.db.collection('/competitions').valueChanges()
-  }
+	createCompetition(comp_name: string, breed: string) {
+		//  console.log(comp_name);
+		this.comp_name = comp_name;
+		this.difficulty = this.getRandStats();
+		this.energy = this.getRandStats();
+		this.kitty = this.getRandValue();
+		this.ranks = this.getRandRank();
+		this.over = false;
 
-  createCompetition (comp_name:string, breed:string) {
-    //  console.log(comp_name);
-      this.comp_name = comp_name;
-      this.difficulty = this.getRandStats();
-      this.energy = this.getRandStats();
-      this.kitty = this.getRandValue();
-      this.ranks = this.getRandRank();
-      this.over = false;
-        
-        return this.db.collection('competitions').add({
-        comp_name: comp_name,
-        difficulty: this.difficulty,
-        energy: this.energy,
-        kitty: this.kitty,
-        ranks: this.ranks,
-        breed: breed,
-        over: this.over,
-      })
-    }
+		this.db.collection('competitions').add({
+			comp_name: comp_name,
+			difficulty: this.difficulty,
+			energy: this.energy,
+			kitty: this.kitty,
+			ranks: this.ranks,
+			breed: breed,
+			over: this.over
+		});
+	}
 
-  getRandStats(): number {
-    return Math.floor(Math.random() * 10 + 10);
-  }
+	getRandStats(): number {
+		return Math.floor(Math.random() * 10 + 10);
+	}
 
-  getRandValue(): number {
-    return Math.floor(Math.random() * 1000 + 1);
-  }
+	getRandValue(): number {
+		return Math.floor(Math.random() * 1000 + 1);
+	}
 
-  getRandRank(): number {
-    return Math.floor(Math.random() * 10);
-  }
-
-  
-
+	getRandRank(): number {
+		return Math.floor(Math.random() * 10);
+	}
 }
