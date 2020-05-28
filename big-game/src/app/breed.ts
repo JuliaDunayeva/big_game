947a6908d9bb47c56b5e02a25e1cc3903d46f613
mojfazel <fazeli.mojtaba@gmail.com>
2020-05-28 14:34:48 -0400

    adding doc_id

--- big-game/src/app/breed.ts
+++ big-game/src/app/breed.ts
@@ -1,11 +1,13 @@
 export class Breed {
+	private doc_id: string;
 	private key: string;
 	private breed: string;
 	private breed_id: number;
 	private skill: string;
 	private img_path: string;
 
-	constructor(key: string, breed: string, breed_id: number, skill: string, img_path: string) {
+	constructor(doc_id: string, key: string, breed: string, breed_id: number, skill: string, img_path: string) {
+		this.doc_id = doc_id;
 		this.key = key;
 		this.breed = breed;
 		this.breed_id = breed_id;
