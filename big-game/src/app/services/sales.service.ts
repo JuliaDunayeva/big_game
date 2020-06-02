0a85a8636c06be89f4e63d72174e5d7fe16bc797
Gervan <gervan@users.noreply.github.com>
2020-06-02 11:12:28 -0400

    horse list updates

--- big-game/src/app/services/sales.service.ts
+++ big-game/src/app/services/sales.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
