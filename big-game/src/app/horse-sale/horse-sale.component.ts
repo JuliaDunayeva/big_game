813807868014365c8511da8b2daf191242b41d89
Gervan <gervan@users.noreply.github.com>
2020-06-08 20:36:32 -0400

    adding to buy horse

--- big-game/src/app/horse-sale/horse-sale.component.ts
+++ big-game/src/app/horse-sale/horse-sale.component.ts
@@ -3,7 +3,13 @@ import { UserData } from '../user-data';
 import { UserDataService } from '../services/user-data.service';
 import { HorseData } from '../horse-data';
 import { HorseDataService} from '../services/horse-data.service';
+import { SalesService } from '../services/sales.service';
+import { BreedService } from '../services/breed.service';
+import { Breed } from '../breed';
+import { ColorService } from '../services/color.service';
+import { Color } from '../color';
 import { AuthService } from '../services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-horse-sale',
@@ -11,10 +17,67 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./horse-sale.component.css']
 })
 export class HorseSaleComponent implements OnInit {
+  allHorses: Breed[];
+  allHorseData: Array<HorseData>;
+  horse: HorseData;
+  horseValues: { name, breed, color };
+  selectHorse: FormGroup
+  defaultHorse: any;
+  horseSelectedId: string;
+  Uid: string = this.authService.getUId();
+  user: any;
+  breedService: any;
+  colorService: any;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+        private userDataService: UserDataService,
+        private horseDataService: HorseDataService,
+        private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+      this.getHorseData();
+      this.userDataService.getUserByID(this.Uid).subscribe((result) => {
+        this.user = result as UserData;
+      });
+    }
+
+    getHorseData(){
+      this.horseDataService.getHorsesByUid().subscribe(
+        res => {
+          this.allHorseData = res as Array<HorseData>;
+          this.allHorseData.map(horse =>{
+            this.defaultHorse = this.allHorseData[0].name
+            this.horseSelectedId = this.allHorseData[0].id
+            this.createForm();
+            this.breedService.getBreedById(horse.breed).then( res =>{
+              horse.breed = res.data()['breed']}
+              )
+            }
+          )
+          this.allHorseData.map(horse =>{
+            this.colorService.getColorById(horse.color).then( res =>
+              horse.color = res.data()['color']
+              )
+            }
+          )
+        }
+      )
+    }
+
+    createForm() {
+      console.log(this.defaultHorse)
+      this.selectHorse = this.fb.group({
+        myHorse: [this.defaultHorse, Validators.required]
+      })
+    }
+  
+    selectedHorse(event: any) {
+      console.log("test   ",(<HTMLInputElement>event.target).id)
+      this.horseSelectedId = (<HTMLInputElement>event.target).id;
+    }
+  
+    onSelectHorse() {
+      this.authService.setHorseId(this.horseSelectedId)
+    }
 
 }
--- big-game/src/app/services/sales.service.ts
+++ big-game/src/app/services/sales.service.ts
@@ -2,11 +2,24 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
 import { Observable, from } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { HorseData } from '../horse-data';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
 
-  constructor() { }
+  constructor(public db: AngularFirestore,
+    private authService:AuthService) { }
+    
+    getHorseByID(id : string) : Observable<HorseData> {
+		return this.db.collection('/horse_data').doc(id).snapshotChanges().pipe(
+			map(res => { 
+				const horse = res.payload.data() as HorseData;
+				horse.id = res.payload.id;
+				return horse;
+			})			
+		);
+	}//end of getHorsesByID()
 }
