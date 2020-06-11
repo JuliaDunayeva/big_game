c77cad0c45593a30a69921cb1089c2a22084c204
Gervan <56656516+gervanpn@users.noreply.github.com>
2020-06-11 09:06:31 -0400

    Update app-routing.module.ts

--- big-game/src/app/app-routing.module.ts
+++ big-game/src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { HorsePageComponent } from './horse-page/horse-page.component';
 import { CompetitionsComponent } from './competition-page/competitions/competitions.component';
 import { HorseListComponent } from './horse-list/horse-list.component';
 
-
 const routes: Routes = [
   {
     path:'sign-up',
@@ -26,9 +25,7 @@ const routes: Routes = [
    {
     path:'home-page',
     component: HomePageComponent
-
   },
-  
   {
     path: 'store-page',
     component: StorePageComponent
