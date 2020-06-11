59982c23fc44858d855330091ba982740d3b4402
drThom <drthomsa@gmail.com>
2020-06-11 09:33:22 -0400

    tack pg changes

--- big-game/src/app/store-page/tack-page/tack-page.component.ts
+++ big-game/src/app/store-page/tack-page/tack-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tack-page.component.css']
 })
 export class TackPageComponent implements OnInit {
-
+  public allHorses = [];
   constructor() { }
 
   ngOnInit(): void {
