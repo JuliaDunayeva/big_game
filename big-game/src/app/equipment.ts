585adf11f76afec3cb21811d050092680399c53d
RoaaAlmadi <roaa.almadi@gmail.com>
2020-06-17 16:28:43 -0700

    try to dissplay on the page (not fully working yet)

--- big-game/src/app/equipment.ts
+++ big-game/src/app/equipment.ts
@@ -26,3 +26,33 @@ export class Equipment {
         this.trot_ = trot_;
     }
 }
+
+export class saddleIdList {
+        name: string;
+        color: string;
+        equipment: string;
+        img_file: string;
+        id: string;
+        dressage_: number;
+        gallop_: number;
+        jumping_: number;
+        speed_: number;
+        stamina_: number;
+        trot_: number;
+        
+    constructor(name: string, color: string, equipment: string, img_file: string, id: string,
+        dressage_: number, gallop_: number, jumping_: number, speed_: number, stamina_: number, trot_: number) {
+        this.name = name;
+        this.color = color;
+        this.equipment = equipment;
+        this.img_file = img_file;
+        this.id = id;
+        this.dressage_ = dressage_;
+        this.gallop_ = gallop_;
+        this.jumping_ = jumping_;
+        this.speed_ = speed_;
+        this.stamina_ = stamina_;
+        this.trot_ = trot_;
+    }
+}
+
