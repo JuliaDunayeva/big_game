ce08c57bfe6cd3fa9b09d5e984c1b839f07cf479
Gervan <gervan@users.noreply.github.com>
2020-06-26 11:28:20 -0400

    commented out console.logs in ts files

--- big-game/src/app/equipment.ts
+++ big-game/src/app/equipment.ts
@@ -30,32 +30,3 @@ export class Equipment {
     }
 }
 
-export class saddleIdList {
-        name: string;
-        color: string;
-        equipment: string;
-        img_file: string;
-        id: string;
-        dressage_: number;
-        gallop_: number;
-        jumping_: number;
-        speed_: number;
-        stamina_: number;
-        trot_: number;
-        
-    constructor(name: string, color: string, equipment: string, img_file: string, id: string,
-        dressage_: number, gallop_: number, jumping_: number, speed_: number, stamina_: number, trot_: number) {
-        this.name = name;
-        this.color = color;
-        this.equipment = equipment;
-        this.img_file = img_file;
-        this.id = id;
-        this.dressage_ = dressage_;
-        this.gallop_ = gallop_;
-        this.jumping_ = jumping_;
-        this.speed_ = speed_;
-        this.stamina_ = stamina_;
-        this.trot_ = trot_;
-    }
-}
-
