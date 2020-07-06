7419a1849793873ab5ca72d0c440f122e2b7aa31
drThom <drthomsa@gmail.com>
2020-07-06 18:05:46 -0400

    Added saddle purchase to DB

--- big-game/src/app/store-page/tack-page/tack-data/tack-data.component.ts
+++ big-game/src/app/store-page/tack-page/tack-data/tack-data.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { SaddlesService } from '../../../services/saddles.service';
 import { UserDataService } from '../../../services/user-data.service';
-import { HorseDataService } from '../../../services/horse-data.service';
 import { AuthService } from '../../../services/auth.service';
-import { UserData } from 'src/app/user-data';
 import { Equipment } from 'src/app/equipment';
+import { TackService } from 'src/app/services/tack.service';
+
 
 @Component({
   selector: 'app-tack-data',
@@ -17,12 +16,12 @@ export class TackDataComponent implements OnInit {
   Uid: string = this.authService.getUId();
   userEquus: number;
   buyCost: number;
+  horse_id: string = this.authService.getHorseId();
 
 
   constructor(private userService: UserDataService, 
               private authService: AuthService, 
-              private saddlesService: SaddlesService, 
-              private horseService: HorseDataService) { }
+              private tackService: TackService) { }
 
   public imgpath = "assets/images/tack-page/";
 
@@ -30,65 +29,26 @@ export class TackDataComponent implements OnInit {
     return this.imgpath + file;
   }
   ngOnInit(): void {
+    console.log('begining')
     this.userService.getUserByID(this.Uid).subscribe((result) => {
       // console.log('user info', result)
       this.userEquus = result.equus
       console.log('equus', this.userEquus)
     })
   }
-  
-  buyTack(tackCost) {
-    this.costCheck(tackCost)
-    console.log('cost',tackCost )
-  } 
-  // Cost() {
-  //   this.userService.subtractEquus(this.Uid, this.user.equus, this.equipment.cost)
-    
-  // } // used to buy a new Tack and pay X Equus
-
 
   haveMoney: boolean;
-  costCheck(cost) {
-    if (this.userEquus >= cost) {
-        this.userService.subtractEquus(this.Uid, this.userEquus, cost );   
+  costCheck(equipment: Equipment) {
+    if (this.userEquus >= equipment.cost) {
+        this.userService.subtractEquus(this.Uid, this.userEquus, equipment.cost ); 
+        this.tackService.buyTack(this.horse_id, equipment.saddleId, equipment.cost)
+          alert('You have bought the item')
+  
     } else {
-      alert('Not enough equus')
+      alert('Not enough equus to buy this item.')
     } 
     // used to buy a new Tack and pay X Equus
   }
 
-  
 
-  //     haveMoney: boolean;
-  //     this.costCheck() {
-  //       if (this.user.equus >= ) {
-  //         this.Cost();
-  //         return this.haveMoney = true;
-  //       }
-  //       else {
-  //         return this.haveMoney = false;
-  //       }
-  //     }
-  //   this.Cost() {
-  //       this.userService.subtractEquus(this.Uid, this.user.equus, )
-  //     } // used to buy a new Tack and pay X Equus
-    
-  //     haveMoney: boolean;
-  //     this.costCheck() {
-  //       if (this.user.equus >= ) {
-  //         this.Cost();
-  //         return this.haveMoney = true;
-  //       }
-  //       else {
-  //         return this.haveMoney = false;
-  //       }
-  //     }
-  //   this.Cost();
-  //     return this.haveMoney = true;
-  //   }
-  //   else {
-  //     return this.haveMoney = false;
-  //   }
-  // }
-  // Uid(Uid: any, equus: any) { }
 }
