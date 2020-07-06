84fe10c50f53573a061b7c703e056a533214ea3d
aus <ausalmadi@gmail.com>
2020-07-06 13:11:41 -0400

    profile page

--- big-game/src/app/app-routing.module.ts
+++ big-game/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProfileComponent } from './profile/profile.component';
 import { CompetitionPageComponent } from './competition-page/competition-page.component';
 import { BlackMarketPageComponent } from './black-market-page/black-market-page.component';
 import { StorePageComponent } from './store-page/store-page.component';
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'sign-up',
     component: SignUpComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   },
   {
     path: 'horse-list',
