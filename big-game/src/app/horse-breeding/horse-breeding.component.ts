5f85e8fd715586a31b83829f0cf93c7d3f3aff7f
lalaantika <sharmal0707@gmail.com>
2020-07-06 12:27:00 -0400

    some fixes

--- big-game/src/app/horse-breeding/horse-breeding.component.ts
+++ big-game/src/app/horse-breeding/horse-breeding.component.ts
@@ -27,7 +27,8 @@ export class HorseBreedingComponent implements OnInit {
   stallionUserInfo:UserData;
   Uid: string = this.authService.getUId();
   public mareBreedId: string;
-  public marecolorId: string;
+  public mareColorId: string;
+  public stallionBreed: string;
   public imagePath: string;
   public horse: HorseData = new HorseData;
   public stallionUserEquus:number;
@@ -54,7 +55,7 @@ export class HorseBreedingComponent implements OnInit {
     this.horseService.getHorseById(this.id).subscribe((res) => {
       this.mareData = res as HorseData;
       this.mareBreedId = this.mareData.breed;
-      this.marecolorId = this.mareData.color;
+      this.mareColorId = this.mareData.color;
       console.log('first',this.mareBreedId);
       this.breedService.getBreedById(this.mareData.breed).then(brd => {
         this.mareData.breed = brd.data()['breed'];
@@ -91,8 +92,9 @@ export class HorseBreedingComponent implements OnInit {
   }
 
 
-  selectedStallion(userID:string){
+  selectedStallion(userID:string, breedId:string){
     if (this.haveMoney == true) {
+      this.stallionBreed = breedId;
         this.userService.getUserByID(userID).subscribe((result) => {
           this.stallionUserInfo = result as UserData;
           this.stallionUserEquus = this.stallionUserInfo.equus;
@@ -159,7 +161,7 @@ export class HorseBreedingComponent implements OnInit {
 
   createBaby(name:string) {
     if (this.haveMoney == true) {
-      this.horseService.newBaby( this.Uid, this.mareBreedId, this.mareBreedId, this.mareData.skill, name)
+      this.horseService.newBaby( this.Uid, this.stallionBreed, this.mareColorId, this.mareData.skill, name)
       this.newHorseCost();
       return alert(this.success);
     } alert(this.fail)
