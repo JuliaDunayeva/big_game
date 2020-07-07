53b79e55cb8e0d1011e910e776e31655fb4d03fb
burningman2k20 <burningman2k9@gmail.com>
2020-07-07 00:35:06 -0400

    updated eq page

--- big-game/src/app/eqcenter-register/eqcenter-register.component.ts
+++ big-game/src/app/eqcenter-register/eqcenter-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HorseData } from '../horse-data';
 import { HorseDataService } from '../services/horse-data.service';
 import { AuthService } from '../services/auth.service';
@@ -17,20 +18,34 @@ export class EqcenterRegisterComponent implements OnInit {
   public id: string;
   alleqCenters: Array<EqCenters>;
   public selected: boolean = true;
+  selectEQCenter: FormGroup
 
-  constructor( public horseDataService: HorseDataService, private authService: AuthService, private eqcenterService: EqCenterService) {
+  constructor( public horseDataService: HorseDataService, private authService: AuthService, private eqcenterService: EqCenterService,  private fb: FormBuilder) {
       this.id = this.authService.getHorseId();
     }
 
   ngOnInit(): void {
-    this.showCompetitions();
+    this.showEqCenters();
+    this.createForm();
   }
 
-  showCompetitions() {
+  createForm() {
+    this.selectEQCenter = this.fb.group({
+      eqSelect: ['something', Validators.required]
+    })
+  }
+
+  showEqCenters() {
 		this.eqcenterService.geteqCenters().subscribe(res => {
 			this.alleqCenters = res as unknown as Array<EqCenters>;
 			//console.log('comps ', this.allCompetitions)
 		})
-	}
+  }
+  
+  selectEQ(event: any){
+    //let something = (<HTMLInputElement>event.target).id;
+    //console.log(something);
+    console.log('clicked');
+  }
 
 }
